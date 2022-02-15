b0a349dda509ed89e48d1736c38800ac67137363
Milanparajuli <milanbhai2058@gmail.com>
2022-02-15 07:21:47 +0545

    initialize commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { RightComponent } from './right/right.component';
+import { LeftComponent } from './left/left.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    RightComponent,
+    LeftComponent
   ],
   imports: [
     BrowserModule,
